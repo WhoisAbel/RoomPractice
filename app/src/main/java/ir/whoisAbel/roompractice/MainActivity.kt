0fc31227c2e250f07ed1af0e14effaefbd6080ac
WhoisAbel <whoisabel1996@gmail.com>
2022-04-24 18:10:34 +0430

    Insert Data (#5)

--- app/src/main/java/ir/whoisAbel/roompractice/MainActivity.kt
+++ app/src/main/java/ir/whoisAbel/roompractice/MainActivity.kt
@@ -2,10 +2,39 @@ package ir.whoisAbel.roompractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+        setupActionBarWithNavController(
+            navController,
+            appBarConfiguration
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration =
+            AppBarConfiguration.Builder()
+                .build()
     }
 }
--- app/src/main/java/ir/whoisAbel/roompractice/user/ui/ListFragment.kt
+++ app/src/main/java/ir/whoisAbel/roompractice/user/ui/ListFragment.kt
@@ -5,16 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ir.whoisAbel.roompractice.R
+import ir.whoisAbel.roompractice.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+
+        initValue()
+    }
+
+    private fun initView() {
+        binding.fbAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+    }
+
+    private fun initValue() {
+
     }
 
 }
